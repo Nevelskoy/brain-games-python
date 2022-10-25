19af618a8951b14427b1c2f53bc305098e5ab3c4
Sadovskiy <safelikc@gmail.com>
2022-10-25 08:24:22 +0300

    fix brain-gcd

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -8,6 +8,7 @@ DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 def get_parity(number):
     return 'no' if number % 2 else 'yes'
 
+
 def make_question():
     question = get_random_number()
     correct_answer = get_parity(question)
--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -5,9 +5,14 @@ from brain_games.scripts.utils import get_random_number
 DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 
+def get_gcd(first, second):
+    if second == 0: return first
+    return get_gcd(second, first % second) 
+
+
 def make_question():
     first = get_random_number()
     second = get_random_number()
     question = f'{first} {second}'
-    correct_answer = 
+    correct_answer = str(get_gcd(first, second))
     return question, correct_answer
