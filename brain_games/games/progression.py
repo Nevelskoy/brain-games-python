3bc5a228db0c524c5cb199b24139a1993d408552
Nevelskoy <safelikc@gmail.com>
2022-10-26 19:08:03 +0300

    fix brain-progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -7,7 +7,7 @@ LENGTH_PROGRESSION = 10
 
 def get_crux(progression):
     progression_crux = ''
-    hidden_index = get_random_number(1, LENGTH_PROGRESSION - 1)
+    hidden_index = get_random_number(1, LENGTH_PROGRESSION - 2)
     hidden_num = str(progression[hidden_index])
     progression[hidden_index] = '..'
     for item in progression:
