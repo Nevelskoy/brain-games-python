2c83b5d2df3d7477ed158b6db7ee1d98821d5180
Nevelskoy <safelikc@gmail.com>
2022-10-26 15:49:21 +0300

    fix import

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.games import brain_calc
+from brain_games.games import calc
 from brain_games.engine import run
 
 
 def main():
-    run(brain_calc)
+    run(calc)
 
 
 if __name__ == '__main__':
