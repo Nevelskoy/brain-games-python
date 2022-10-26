61cd0b77a29194e2d2df82b6be95987fb51e2185
Sadovskiy <safelikc@gmail.com>
2022-10-26 08:16:20 +0300

    fix brain_progression

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -5,25 +5,26 @@ from brain_games.scripts.utils import get_random_number
 DESCRIPTION = 'What number is missing in the progression?'
 
 
-def get_progression():
-    '''
-    l = []
-    start = 1
-    step = 5
-    while len(l) < 10:
+def get_crux(progression):
+    progression_crux = ''
+    hidden_index = get_random_number(1, 9)
+    hidden_num = str(progression[hidden_index])
+    progression[hidden_index] = '..'
+    for item in progression:
+        progression_crux += f' {str(item)}'
+    return progression_crux, hidden_num
+
+
+def get_progression(start, step):
+    progression = []
+    while len(progression) < 10:
         start += step
-        l.append(start)
-    res = ''
-    for num in l:
-        res += f' {str(num)}'
-    print(l)
-    print(res)
-    '''
+        progression.append(start)
+    return progression
 
 
 def make_question():
     start = get_random_number()
     step = get_random_number(1, 10)
-    question = get_progression()
-    correct_answer = get_progression()
+    question, correct_answer = get_crux(get_progression(start, step))
     return question, correct_answer
